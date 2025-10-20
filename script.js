30545562d99aa48b818b5e27231331475c38b44d
the-phoenix-coder <ahmedhesham.dev@gmail.com>
2025-10-20 20:44:58 +0300

    style: develope icons flash and performance by using css instead of js

--- script.js
+++ script.js
@@ -7,41 +7,10 @@ const logoIcon = document.getElementById("logo")
 
 const THEME_KEY = 'site-theme';
 
-const icons = {
-    light: {
-        Sun: "./Images/icons/Sun.svg",
-        Github: "./Images/icons/Github.svg",
-        Linkedin: "./Images/icons/LinkedIn.svg",
-        Logo: "./Images/icons/Size=Small, Mode=Dark.svg",
-    },
-    dark: {
-        Sun: "./Images/icons/Sun.png",
-        Github: "./Images/icons/Github.png",
-        Linkedin: "./Images/icons/Linkedin.png",
-        Logo: "./Images/icons/Property 1=white small.svg",
-    }
-}
-
-function lightMood()
-{
-    sunIcon.src = icons.light.Sun;
-    githubIcon.src = icons.light.Github;
-    linkedinIcon.src = icons.light.Linkedin;
-    logoIcon.src = icons.light.Logo;
-}
-function darkMood()
-{
-    sunIcon.src = icons.dark.Sun;
-    githubIcon.src = icons.dark.Github;
-    linkedinIcon.src = icons.dark.Linkedin;
-    logoIcon.src = icons.dark.Logo;
-}
-
 let savedTheme = localStorage.getItem(THEME_KEY)
 
 if (savedTheme === 'true'){
     htmlEle.classList.add('light');
-    lightMood()
 }
 
 themeToggle.addEventListener('click', () =>
@@ -49,12 +18,4 @@ themeToggle.addEventListener('click', () =>
     htmlEle.classList.toggle('light');
     isLight = htmlEle.classList.contains('light');
     localStorage.setItem(THEME_KEY, isLight)
-
-    if (htmlEle.classList.contains('light'))
-    {
-        lightMood()
-    } else
-    {
-        darkMood()
-    }
 })
