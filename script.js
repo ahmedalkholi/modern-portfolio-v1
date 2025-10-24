7de90651588d4f79e3758168f7c68eef9140e9ed
the-phoenix-coder <ahmedhesham.dev@gmail.com>
2025-10-24 20:49:05 +0300

    style: added responsive design for the header and made it mobile friendly

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ themeToggle.addEventListener('click', () =>
     isLight = htmlEle.classList.contains('light');
     localStorage.setItem(THEME_KEY, isLight)
 })
+
+// Start active link effects
+
+const links = document.querySelectorAll('.nav-icon-container a')
+const currentPath = window.location.pathname
+
+
+function setAllToLine()
+{
+    links.forEach(l =>
+        {
+            const activeIcon = l.querySelector('i')
+            activeIcon.className = activeIcon.className.replace('-fill', '-line')
+        })
+}
+
+function setFill(link)
+{
+    const i = link.querySelector('i')
+    i.className = i.className.replace(/-line\b/g, '-fill');
+}
+
+setAllToLine()
+
+links.forEach(link =>
+{
+    link.addEventListener('click', () =>
+    {
+        setAllToLine()
+        setFill(link)
+    })
+})
+
+links.forEach(link =>
+{
+    const icon = link.querySelector('i')
+    icon.className = icon.className.replace('-fill', '-line')
+    if (currentPath.includes(link.getAttribute('href')))
+    {
+        icon.className = icon.className.replace('-line', '-fill')
+    }
+})
+
+
+links.forEach(link =>
+{
+    if (location.hash === link.getAttribute('href'))
+    {
+        setFill(link)
+    }
+})
